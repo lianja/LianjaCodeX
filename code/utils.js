38e775469a48e9b17b5577cf31059389718763d3
utils.js <support@lianja.com>
2025-06-25 12:47:26 +0100

    Latest update

---
+++ code/utils.js
@@ -0,0 +1,16 @@
+// example JavaScript Module
+//
+// var utils = require("./utils");
+// utils.myfunc()
+// utils.myfunc2()
+//
+
+var myfunc = function() {
+    // myfunc code
+};
+var myfunc2 = function() {
+    // myfunc2 code
+};
+module.exports.myfunc = myfunc;
+module.exports.myfunc2 = myfunc2;
+
